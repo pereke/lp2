2b99e8e2bec0b4397a6c6c3cfb2ae8fa9d32a263
Pedro Henrique Costa <pereke007@gmail.com>
2015-03-26 14:31:09 -0300

    Create Exponencial.java

---
+++ Exercicio2/Exponencial.java
@@ -0,0 +1,8 @@
+public class Exponencial{
+  public static void main(){
+    for (int = i; i< 10; i++){
+      System.out.println (i + "  " + (int)Math.pow(i, 2) + "  " + (int)Math.pow(i, 3));
+    }
+  }
+}
+  
